38f751e3152a3f4dea36c88073ec1185a2c58475
vj-abishek <abigosearch@gmail.com>
2020-10-28 10:10:55 +0530

    Changed the third party library

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-    let script1 = document.createElement('script');
+    const script1 = document.createElement('script');
     script1.src = chrome.runtime.getURL('injectScript.js')
     document.head.appendChild(script1);
-    let script2 = document.createElement('script');
+    const script2 = document.createElement('script');
 
-    // https://raw.githubusercontent.com/MrRio/jsPDF/v2.1.1/dist/jspdf.umd.min.js
-    script2.src = chrome.runtime.getURL('lib/2.1.1/jspdf.umd.min.js');
+    // https://raw.githubusercontent.com/MrRio/jsPDF/v2.1.1/dist/jspdf.umd.js
+    script2.src = chrome.runtime.getURL('lib/2.1.1/jspdf.umd.js');
     document.head.appendChild(script2);
 }
